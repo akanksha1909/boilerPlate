d370976d1eed208a37b4684cbd909dabaef5021e
Akanksha <akanksha1909singh@gmail.com>
2018-07-22 00:07:48 +0530

    updated

--- app.js
+++ app.js
@@ -1,22 +1,88 @@
-	const express = require('express');  // Express app library
-	const mongoose  = require('mongoose'); // Database layer for mongodb
-	const bodyParser = require('body-parser'); // Body parser for parsing input parameters in request
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const app = express();
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const RateLimit = require('express-rate-limit');
 
-	const app = express();
+// routes variable
+// const TestRoutes = require();
 
-	mongoose.set('debug',true);
-	mongoose.connect('mongodb://localhost:27017/bookmarks');
+switch (process.env.mode) {
+    case 'development':
+        process.env.PORT = process.env.DevPort;
+        process.env.DATABASE = process.env.DevDatabase;
+        break;
+    case 'local':
+        process.env.PORT = process.env.LocalPort;
+        process.env.DATABASE = process.env.LocalDatabase;
+        break;
+    case 'test':
+        process.env.PORT = process.env.TestPort;
+        process.env.DATABASE = process.env.TestDatabase;
+        break;
+    default:
+        //production
+        process.env.PORT = process.env.ProdPort;
+        process.env.DATABASE = process.env.ProdDatabase;
+        break;
+}
 
+app.use(logger('dev'));
 
-	//Initialize plugins
-	app.use(bodyParser.json( {limit: '50mb'} )); // support json encoded bodies
-	app.use(bodyParser.urlencoded({ extended: true,limit: '50mb', parameterLimit: 100000 })); // support encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-	const user = require('./routes/userRouter.js');
-	app.use('/user',user);
+// used for stopping header attacks
+app.use(helmet());
 
-	const server = app.listen(5000,function(){
-		console.log('Server is running on Port: 5000');
-	});
+// To stop mongodb injection 
+app.use(mongoSanitize());
 
-	module.exports = server;
+// To prevent from brute force attack
+const limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 600, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
+
+app.use(limiter);
+
+mongoose.connect(process.env.DATABASE,{useNewUrlParser: true});
+
+// When successfully connected
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + process.env.DATABASE);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+if (process.env.mode != 'production') {
+    console.log("MODE => " + process.env.mode);
+    console.log("PORT => " + process.env.PORT);
+    console.log("Database => " + process.env.DATABASE);
+}
+
+//routes
+// app.use('/test', TestRoutes)
+
+app.use((req, res, next) => {
+    res.send('Wrong Url');
+})
+console.log(process.env.PORT)
+const server = app.listen(process.env.PORT, function () {
+    console.log('Server is running on host 5000');
+})
+
+module.exports = server;
